8531f864ff6cd70e9451d159c123bf2747ec9031
matthiasgreen <matthiasgreen2@gmail.com>
2025-02-03 13:41:53 +0100

    Fixes to fourier sliders

--- src/pages/fourier/FourierProject.tsx
+++ src/pages/fourier/FourierProject.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { SyntheticEvent, useEffect, useRef, useState } from "react";
 import PageTabs from "../../components/page/PageTabs";
 import "./FourierProject.css";
 import { FourierAnimation } from "fourier-animation";
@@ -8,8 +8,7 @@ import { Slider, Stack, Typography } from "@mui/material";
 function Presentation() {
     return (
         <>
-            <h2> Introduction </h2>
-            <p> Coming soon </p>
+            <h2> Article coming soon, check out the demonstration </h2>
         </>
     )
 }
@@ -35,28 +34,29 @@ function Demonstration() {
         animate();
     }, []);
 
-    const changeSeriesSize = (event: Event, value: number | number[]) => {
+    const changeSeriesSize = (_event: SyntheticEvent | Event, value: number | number[]) => {
+        console.log("here");
         if (!animation) throw new Error("Animation not initialized");
         animation.updateParams({
             seriesSize: value as number
         })
     };
 
-    const changeHistoryLength = (event: Event, value: number | number[]) => {
+    const changeHistoryLength = (_event: SyntheticEvent | Event, value: number | number[]) => {
         if (!animation) throw new Error("Animation not initialized");
         animation.updateParams({
             historyLength: value as number
         })
     };
 
-    const changeNSamples = (event: Event, value: number | number[]) => {
+    const changeNSamples = (_event: SyntheticEvent | Event, value: number | number[]) => {
         if (!animation) throw new Error("Animation not initialized");
         animation.updateParams({
             nSamples: value as number
         })
     };
 
-    const changeSpeed = (event: Event, value: number | number[]) => {
+    const changeSpeed = (_event: SyntheticEvent | Event, value: number | number[]) => {
         if (!animation) throw new Error("Animation not initialized");
         animation.updateParams({
             speed: value as number
@@ -68,53 +68,55 @@ function Demonstration() {
             <Infobox><span>Click and drag to draw something!</span></Infobox>
             <Stack spacing={2} direction="row">
                 <Stack spacing={2} direction="column">
-                    <Typography>Series size</Typography>
-                    <Slider 
-                        onChange={changeSeriesSize}
-                        defaultValue={30}
-                        min={1}
-                        max={100}
-                        step={1}
-                        valueLabelDisplay="auto"
-                    />
+                    <Stack spacing={2} direction="column">
+                        <Typography>Series size</Typography>
+                        <Slider 
+                            onChangeCommitted={changeSeriesSize}
+                            defaultValue={30}
+                            min={1}
+                            max={100}
+                            step={1}
+                            valueLabelDisplay="auto"
+                        />
+                    </Stack>
+                    <Stack spacing={2} direction="column">
+                        <Typography>History length</Typography>
+                        <Slider 
+                            onChange={changeHistoryLength}
+                            defaultValue={10}
+                            min={1}
+                            max={50}
+                            step={1}
+                            valueLabelDisplay="auto"
+                        />
+                    </Stack>
+                    <Stack spacing={2} direction="column">
+                        <Typography>N samples</Typography>
+                        <Slider 
+                            onChangeCommitted={changeNSamples}
+                            defaultValue={100}
+                            min={1}
+                            max={500}
+                            step={1}
+                            valueLabelDisplay="auto"
+                        />
+                    </Stack>
+                    <Stack spacing={2} direction="column">
+                        <Typography>Speed</Typography>
+                        <Slider 
+                            onChange={changeSpeed}
+                            defaultValue={20}
+                            min={1}
+                            max={100}
+                            step={1}
+                            valueLabelDisplay="auto"
+                        />
+                    </Stack>
                 </Stack>
-                <Stack spacing={2} direction="column">
-                    <Typography>History length</Typography>
-                    <Slider 
-                        onChange={changeHistoryLength}
-                        defaultValue={10}
-                        min={1}
-                        max={50}
-                        step={1}
-                        valueLabelDisplay="auto"
-                    />
-                </Stack>
-                <Stack spacing={2} direction="column">
-                    <Typography>N samples</Typography>
-                    <Slider 
-                        onChange={changeNSamples}
-                        defaultValue={100}
-                        min={1}
-                        max={500}
-                        step={1}
-                        valueLabelDisplay="auto"
-                    />
-                </Stack>
-                <Stack spacing={2} direction="column">
-                    <Typography>Speed</Typography>
-                    <Slider 
-                        onChange={changeSpeed}
-                        defaultValue={20}
-                        min={1}
-                        max={100}
-                        step={1}
-                        valueLabelDisplay="auto"
-                    />
-                </Stack>
+                <div ref={containerRef} className="canvas-container">
+                    <canvas ref={canvasRef} ></canvas>
+                </div>
             </Stack>
-            <div ref={containerRef} className="canvas-container">
-                <canvas ref={canvasRef} ></canvas>
-            </div>
         </>
     )
 }
